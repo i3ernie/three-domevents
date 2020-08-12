7de93f71989ae92ebb0462bd2a3804cc604e7d9e
bernie <mail@javaner.de>
2020-08-12 09:45:01 +0200

    static fnc _getRelativMouseXY

--- src/Domevents.es.js
+++ src/Domevents.es.js
@@ -77,6 +77,52 @@ const logEvent = function( ev ){
 		console.log( ev.type+" - "+ev.target.name );
 };
 
+const _getRelativeMouseXY = function( domEvent ) {
+
+	let element = domEvent.target || domEvent.srcElement;
+
+	if (element.nodeType === 3) {
+		element = element.parentNode; // Safari fix -- see http://www.quirksmode.org/js/events_properties.html
+	}
+
+	//get the real position of an element relative to the page starting point (0, 0)
+	//credits go to brainjam on answering http://stackoverflow.com/questions/5755312/getting-mouse-position-relative-to-content-area-of-an-element
+	let elPosition	= { x : 0 , y : 0};
+	let tmpElement	= element;
+	//store padding
+	let style	= getComputedStyle(tmpElement, null);
+	elPosition.y += parseInt(style.getPropertyValue("padding-top"), 10);
+	elPosition.x += parseInt(style.getPropertyValue("padding-left"), 10);
+
+	//add positions
+	do {
+		elPosition.x	+= tmpElement.offsetLeft;
+		elPosition.y	+= tmpElement.offsetTop;
+		style		= getComputedStyle(tmpElement, null);
+
+		elPosition.x	+= parseInt(style.getPropertyValue("border-left-width"), 10);
+		elPosition.y	+= parseInt(style.getPropertyValue("border-top-width"), 10);
+	} while(tmpElement = tmpElement.offsetParent);
+
+	let elDimension	= {
+		width	: (element === window) ? window.innerWidth	: element.offsetWidth,
+		height	: (element === window) ? window.innerHeight	: element.offsetHeight
+	};
+
+	if ( domEvent.type === "touchend" || domEvent.type === "touchstart" ){
+		return {
+			x : +((domEvent.changedTouches[ 0 ].pageX - elPosition.x) / elDimension.width ) * 2 - 1,
+			y : -((domEvent.changedTouches[ 0 ].pageY - elPosition.y) / elDimension.height) * 2 + 1
+		};
+	}
+	else{
+		return {
+			x : +((domEvent.pageX - elPosition.x) / elDimension.width ) * 2 - 1,
+			y : -((domEvent.pageY - elPosition.y) / elDimension.height) * 2 + 1
+		};
+	}
+};
+
 // # Constructor
 const DomEvents = function( camera, domElement )
 {
@@ -159,52 +205,6 @@ Object.assign( DomEvents.prototype,  {
 		this._domElement.removeEventListener( 'touchstart'	, this._$onTouchStart	, false );
 		this._domElement.removeEventListener( 'touchend'	, this._$onTouchEnd		, false );
 		this._domElement.removeEventListener( 'contextmenu'	, this._$onContextmenu	, false );
-	},
-
-
-	_getRelativeMouseXY	: function( domEvent ){
-		let element = domEvent.target || domEvent.srcElement;
-
-		if (element.nodeType === 3) {
-			element = element.parentNode; // Safari fix -- see http://www.quirksmode.org/js/events_properties.html
-		}
-
-		//get the real position of an element relative to the page starting point (0, 0)
-		//credits go to brainjam on answering http://stackoverflow.com/questions/5755312/getting-mouse-position-relative-to-content-area-of-an-element
-		let elPosition	= { x : 0 , y : 0};
-		let tmpElement	= element;
-		//store padding
-		let style	= getComputedStyle(tmpElement, null);
-		elPosition.y += parseInt(style.getPropertyValue("padding-top"), 10);
-		elPosition.x += parseInt(style.getPropertyValue("padding-left"), 10);
-
-		//add positions
-		do {
-			elPosition.x	+= tmpElement.offsetLeft;
-			elPosition.y	+= tmpElement.offsetTop;
-			style		= getComputedStyle(tmpElement, null);
-
-			elPosition.x	+= parseInt(style.getPropertyValue("border-left-width"), 10);
-			elPosition.y	+= parseInt(style.getPropertyValue("border-top-width"), 10);
-		} while(tmpElement = tmpElement.offsetParent);
-
-		let elDimension	= {
-			width	: (element === window) ? window.innerWidth	: element.offsetWidth,
-			height	: (element === window) ? window.innerHeight	: element.offsetHeight
-		};
-
-		if ( domEvent.type === "touchend" || domEvent.type === "touchstart" ){
-			return {
-				x : +((domEvent.changedTouches[ 0 ].pageX - elPosition.x) / elDimension.width ) * 2 - 1,
-				y : -((domEvent.changedTouches[ 0 ].pageY - elPosition.y) / elDimension.height) * 2 + 1
-			};
-		}
-		else{
-			return {
-				x : +((domEvent.pageX - elPosition.x) / elDimension.width ) * 2 - 1,
-				y : -((domEvent.pageY - elPosition.y) / elDimension.height) * 2 + 1
-			};
-		}
 	},
 
 
@@ -632,14 +632,15 @@ Object.assign( DomEvents.prototype,  {
 
 	_onMouseEvent	: function( eventName, domEvent )
 	{
-		let mouseCoords = this._getRelativeMouseXY( domEvent );
+		let mouseCoords = _getRelativeMouseXY( domEvent );
 		this._onEvent(eventName, mouseCoords.x, mouseCoords.y, domEvent);
 		//console.log("RH", eventName, mouseCoords.x, mouseCoords.y, domEvent);
 	},
 
 	_onMouseMove	: function( domEvent )
 	{
-		let mouseCoords = this._getRelativeMouseXY( domEvent );
+		let mouseCoords = _getRelativeMouseXY( domEvent );
+		
 		this._onMove('mousemove', mouseCoords.x, mouseCoords.y, domEvent);
 		this._onMove('mouseover', mouseCoords.x, mouseCoords.y, domEvent);
 		this._onMove('mouseout' , mouseCoords.x, mouseCoords.y, domEvent);
