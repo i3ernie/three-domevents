9ab68e53bf84d0b31bcc80ca27f21297744b6f90
javaner <mail@javaner.de>
2020-08-12 08:32:30 +0200

    Update ExtendedDomevents.js

--- src/ExtendedDomevents.js
+++ src/ExtendedDomevents.js
@@ -1,13 +1,82 @@
 import DomEvents from "./Domevents.es.js";
 
-const ExtendedDomEvents = function( camera, domElement ){
+DomEvents.eventNames.push("drag");
+DomEvents.eventNames.push("dragstart");
+DomEvents.eventNames.push("dragend");
+
+DomEvents.eventMapping.dragstart = "onDragstart";
+DomEvents.eventMapping.dragend = "onDragend";
+DomEvents.eventMapping.drag = "onDrag";
+
+
+const ExtendedDomEvents = function( camera, domElement, opt ){
+
+    this.state = {
+        mousedown : false,
+        dragging : false
+    };
+
+    this._draggingObj = null;
     
     //call parent constructor
-    DomEvents.call( this, camera, domElement );
+    DomEvents.apply( this, arguments );
 };
 
 ExtendedDomEvents.prototype = Object.assign( Object.create( DomEvents.prototype ), {
-    constructor : ExtendedDomEvents
+    constructor : ExtendedDomEvents,
+
+    addEventListener : function( object3d, eventName, callback, opt ){ 
+        let scope = this;
+
+        if ( eventName === "drag" ){
+            object3d.addEventListener("dragstart", function( event ){
+                scope._draggingObj = event.target;
+            });
+            object3d.addEventListener("dragend", function( event ){
+                scope._draggingObj = null;
+            });
+        }
+        DomEvents.prototype.addEventListener.call(this, object3d, eventName, callback, opt);
+    },
+
+    _onMouseDown : function( event ){
+        
+        this.state.mousedown = true;
+        DomEvents.prototype._onMouseDown.call( this, event );
+    },
+
+    _onMouseMove : function( event ){
+        DomEvents.prototype._onMouseMove.call( this, event );
+
+        if ( this.state.mousedown ){
+            if ( !this.state.dragging ) {
+                this.state.dragging = true;
+                this._onMouseEvent('dragstart', event);
+            }
+            this._onMouseEvent('drag', event);
+        }
+    },
+    _onMouseUp : function( event ){
+        
+        DomEvents.prototype._onMouseUp.call( this, event );
+
+        if ( this.state.dragging ){ 
+            console.log("mouseup", this.state.dragging);
+            
+            if ( this._draggingObj ) {
+                this.state.dragging = false;
+                this._notify ( "dragend", this._draggingObj, event, {object:this._draggingObj} );
+	
+            }
+            this._stopDragging();
+        }
+        this.state.mousedown = false;
+        
+    },
+    _stopDragging : function(){
+        this.state.dragging = false;
+        this._onMouseEvent('dragend', event);
+    }
 });
 
 export default ExtendedDomEvents;
