44684f2c6053ffb6b98c23098c4dd0d2eb270c2b
javaner <mail@javaner.de>
2024-01-13 17:31:04 +0100

    BoxBufferGeometry -> BoxGeometry

--- example/js/WoodBox.js
+++ example/js/WoodBox.js
@@ -1,10 +1,10 @@
-import {TextureLoader, BoxBufferGeometry, MeshBasicMaterial, Mesh} from "three";
+import {TextureLoader, BoxGeometry, MeshBasicMaterial, Mesh} from "three";
 
 const texture = new TextureLoader().load( 'textures/crate.gif' );
 
 class WoodBox extends Mesh{ 
     constructor ( w,h,d ) {
-        let geo = new BoxBufferGeometry( w || 200, h || 200, d || 200 );
+        let geo = new BoxGeometry( w || 200, h || 200, d || 200 );
         let mat = new MeshBasicMaterial( { map: texture } );
 
         super( geo,mat );
